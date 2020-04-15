8176f340e277aa0f6ac21dec1076c85cd7755908
SunKyu <leesunkeuy@naver.com>
2020-04-15 17:33:24 +0900

    apply protobuf
    apply protobuf and test

--- api/api.py
+++ api/api.py
@@ -1,8 +1,20 @@
 import time
+import api_pb2 as pb
 from flask import Flask
+from google.protobuf.json_format import MessageToJson
+import json
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/product')
+def get_product():
+    product = pb.Product()
+    product.name = "gana"
+    product.id = 1234
+    product.company = "lotte"
+    product.kcal = 300
+    return MessageToJson(product)
