feee309a847155a645ecdcddc35457f99dbbbf87
SunKyu <leesunkeuy@naver.com>
2020-04-19 16:57:01 +0900

    db_init process

---
+++ api/mongo/db_init.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+import csv
+
+client = MongoClient()
+
+db = client.main_db
+products = db.products
+
+with open('food_list.csv', encoding='utf8') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for line in csv_reader:
+        product = {}
+        product["name"] = line["식품명"]
+        product["company"] = line["제조사"]
+        product["kcal"] = line["에너지(Kcal)"]
+        product["protein"] = line["단백질"]
+        product["fat"] = line["지질"]
+        product["carbs"] = line["탄수화물"]
+        product["sugar"] = line["총당류"]
+        product["na"] = line["나트륨"]
+        product["col"] = line["콜레스테롤"]
+        products.insert_one(product)
+        print(product["name"])
+
+
