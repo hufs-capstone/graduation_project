2e8846b3671544a0c20f8ffa8d35c5066c193a2f
SunKyu <leesunkeuy@naver.com>
2020-04-26 02:43:30 +0900

    test reactstarp

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import { Button } from 'reactstrap';
+import Banner from './components/banner';
+import AppNavbar from './components/navbar';
+
 import './App.css';
 
-function App() {
-  const [current_time, setCurrentTime] = useState(0);
-
-  useEffect(()=> {
-    fetch('/time').then(res=> res.json()).then(data=>{
-        console.log(data);
-      setCurrentTime(data.time);
-    });
-    fetch('/product').then(res=>
-            {
-                console.log(res.json());
-            });
-  }, []);
-  return (
+const App = (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is{current_time}</p>
-        <p>product</p>
+        <AppNavbar/>
+        <h1>WEFWEFWEFEFW</h1>
+        <Button>aaaa</Button>
       </header>
     </div>
-  );
-}
+)
 
 export default App;
