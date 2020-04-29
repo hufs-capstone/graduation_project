c657d026a5efc4d0b9800349e66942ab9f11744e
jenny6585 <jenny6585@naver.com>
2020-04-29 10:41:56 -0700

    bulid

--- api/api.py
+++ api/api.py
@@ -4,7 +4,12 @@ from flask import Flask
 from google.protobuf.json_format import MessageToJson
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build", static_url_path="/")
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/time')
 def get_current_time():
