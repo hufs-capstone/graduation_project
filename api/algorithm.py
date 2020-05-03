d6be56de7c2005a328f7e1aff20eff15ca38f5a0
jenny6585 <jenny6585@naver.com>
2020-05-03 03:01:43 -0700

    transform chart

---
+++ api/algorithm.py
@@ -0,0 +1,37 @@
+table = []
+table['male']={
+
+	"0":{"kcal":550, "protein":10, "fat":11, "carbs":57, "sugar":22, "na":160,"chol": 0},
+	"1":{"kcal":700, "protein":15, "fat":14, "carbs":85, "sugar":29, "na":493,"chol": 0},
+	"2":{"kcal":1000, "protein":15, "fat":20, "carbs":129, "sugar":40, "na":1200,"chol": 0},
+	"3":{"kcal":1400, "protein":20, "fat":21, "carbs":181, "sugar":56, "na":1333,"chol": 0},
+	"4":{"kcal":1700, "protein":30, "fat":30, "carbs":220, "sugar":68, "na":1600,"chol": 0},
+	"5":{"kcal":2100, "protein":40, "fat":41, "carbs":272, "sugar":84, "na":1867,"chol": 0},
+	"6":{"kcal":2500, "protein":55, "fat":54, "carbs":324, "sugar":100, "na":2000,"chol": 0},
+	"7":{"kcal":2700, "protein":65, "fat":64, "carbs":349, "sugar":107, "na":2000,"chol": 0},
+	"8":{"kcal":2600, "protein":65, "fat":61, "carbs":336, "sugar":104, "na":2000,"chol": 3000},
+	"9":{"kcal":2400, "protein":60, "fat":52, "carbs":311, "sugar":96, "na":2000,"chol": 3000},
+	"10":{"kcal":2200, "protein":60, "fat":43, "carbs":283, "sugar":88, "na":2000,"chol": 3000},
+	"11":{"kcal":2000, "protein":55, "fat":34, "carbs":259, "sugar":80, "na":1733,"chol": 3000},
+	"12":{"kcal":2000, "protein":55, "fat":31, "carbs":259, "sugar":80, "na":1467,"chol": 3000}
+
+}
+
+table['female']={
+   
+	"0":{"kcal":550, "protein":10, "fat":11, "carbs":57, "sugar":22, "na":160,"chol": 0},
+	"1":{"kcal":700, "protein":15, "fat":14, "carbs":85, "sugar":29, "na":493,"chol": 0},
+	"2":{"kcal":1000, "protein":15, "fat":20, "carbs":129, "sugar":40, "na":1200,"chol": 0},
+	"3":{"kcal":1400, "protein":20, "fat":21, "carbs":181, "sugar":56, "na":1333,"chol": 0},
+	"4":{"kcal":1500, "protein":25, "fat":23, "carbs":194, "sugar":60, "na":1600,"chol": 0},
+	"5":{"kcal":1800, "protein":40, "fat":32, "carbs":233, "sugar":72, "na":1867,"chol": 0},
+	"6":{"kcal":2000, "protein":50, "fat":39, "carbs":259, "sugar":80, "na":2000,"chol": 0},   
+	"7":{"kcal":2000, "protein":50, "fat":44, "carbs":259, "sugar":80, "na":2000,"chol": 0},   
+	"8":{"kcal":2100, "protein":55, "fat":50, "carbs":273, "sugar":84, "na":2000,"chol": 3000},
+	"9":{"kcal":1900, "protein":50, "fat":41, "carbs":246, "sugar":76, "na":2000,"chol": 3000}, 
+	"10":{"kcal":1800, "protein":50, "fat":35, "carbs":233, "sugar":72, "na":2000,"chol": 3000},
+	"11":{"kcal":1600, "protein":45, "fat":28, "carbs":207, "sugar":64, "na":1467,"chol": 3000},
+	"12":{"kcal":1600, "protein":45, "fat":28, "carbs":207, "sugar":64, "na":1467,"chol": 3000}
+
+}
+
