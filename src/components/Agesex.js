d6785868853d622c81d6812c21ce18dbc485d85d
wooyo2 <37951340+wooyo2@users.noreply.github.com>
2020-05-07 19:29:50 +0900

    Agesex select

--- src/components/Agesex.js
+++ src/components/Agesex.js
@@ -6,14 +6,40 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 const Agesex = (props) =>{
     return(
         <Form>
-            <Form.Row>
-                <FormGroup as={Col} controlId="fromGridAge">
-                    <Form.Control as="select" value="연령대선택">
+            <Row form>
+                <Col>
+                <FormGroup>
+                    <Input type="select" name ="selectAge" id="Age">
+                        <option>연령대 선택</option>
                         <option>0-5 개월</option>
                         <option>6-11 개월</option>
-                    </Form.Control>
+                        <option>1-2 세</option>
+                        <option>3-5 세</option>
+                        <option>6-8 세</option>
+                        <option>9-11 세</option>
+                        <option>12-14 세</option>
+                        <option>15-18 세</option>
+                        <option>19-29 세</option>
+                        <option>30-49 세</option>
+                        <option>50-64 세</option>
+                        <option>65-74 세</option>
+                        <option>75세 이상</option>
+                    </Input>
                 </FormGroup>
-            </Form.Row>
+                </Col>
+                <Col>
+                <FormGroup>
+                    <Input type="select" name ="selectSex" id="Sex">
+                        <option>성별 선택</option>
+                        <option>여성</option>
+                        <option>남성</option>
+                    </Input>
+                </FormGroup>
+                </Col>
+                <Col>
+                    <Button outline color="primary">입력</Button>
+                </Col>
+            </Row>
         </Form>
     )
 }
